a4c0d90abaab67e7189183aa9970ab8ea39d783b
knox-dawson <154367947+knox-dawson@users.noreply.github.com>
2026-04-05 20:04:57 -0400

    Add specs for rm_page

---
+++ spec/io/rm_page_spec.rb
@@ -0,0 +1,165 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+
+RSpec.describe Remarkable::RmPage do
+  def read_varuint(bytes, offset)
+    shift = 0
+    value = 0
+
+    loop do
+      byte = bytes.getbyte(offset)
+      raise "unexpected end of data" unless byte
+
+      offset += 1
+      value |= (byte & 0x7F) << shift
+      break if (byte & 0x80).zero?
+
+      shift += 7
+    end
+
+    [value, offset]
+  end
+
+  def parse_tag(bytes, offset)
+    value, offset = read_varuint(bytes, offset)
+    [{ index: value >> 4, type: value & 0x0F }, offset]
+  end
+
+  def parse_blocks(bytes)
+    offset = Remarkable::RmPage::HEADER_V6.bytesize
+    blocks = []
+
+    while offset < bytes.bytesize
+      size = bytes.byteslice(offset, 4).unpack1("V")
+      block = {
+        size: size,
+        reserved: bytes.getbyte(offset + 4),
+        min_version: bytes.getbyte(offset + 5),
+        current_version: bytes.getbyte(offset + 6),
+        block_type: bytes.getbyte(offset + 7),
+        data: bytes.byteslice(offset + 8, size)
+      }
+      blocks << block
+      offset += 8 + size
+    end
+
+    blocks
+  end
+
+  def parse_subblock(bytes, offset, expected_index:)
+    tag, offset = parse_tag(bytes, offset)
+    expect(tag).to eq(index: expected_index, type: 0x0C)
+
+    size = bytes.byteslice(offset, 4).unpack1("V")
+    offset += 4
+
+    [bytes.byteslice(offset, size), offset + size]
+  end
+
+  def skip_tagged_value(bytes, offset, expected_index:, expected_type:)
+    tag, offset = parse_tag(bytes, offset)
+    expect(tag).to eq(index: expected_index, type: expected_type)
+
+    case expected_type
+    when 0x0F
+      _author, offset = bytes.unpack1("@#{offset}C"), offset + 1
+      _value, offset = read_varuint(bytes, offset)
+      offset
+    when 0x04
+      offset + 4
+    else
+      raise "unsupported tag type #{expected_type}"
+    end
+  end
+
+  def line_value_data(line_block)
+    block_data = line_block[:data]
+    offset = 0
+    offset = skip_tagged_value(block_data, offset, expected_index: 1, expected_type: 0x0F)
+    offset = skip_tagged_value(block_data, offset, expected_index: 2, expected_type: 0x0F)
+    offset = skip_tagged_value(block_data, offset, expected_index: 3, expected_type: 0x0F)
+    offset = skip_tagged_value(block_data, offset, expected_index: 4, expected_type: 0x0F)
+    offset = skip_tagged_value(block_data, offset, expected_index: 5, expected_type: 0x04)
+
+    value_data, = parse_subblock(block_data, offset, expected_index: 6)
+    expect(value_data.getbyte(0)).to eq(0x03)
+    value_data.byteslice(1..)
+  end
+
+  it "writes the expected lines v6 header and top-level block sequence" do
+    page = described_class.new
+    line = page.add_line
+    line.add_point(100, 200)
+
+    bytes = page.to_rm_bytes
+    blocks = parse_blocks(bytes)
+
+    expect(bytes.start_with?(described_class::HEADER_V6)).to be(true)
+    expect(blocks.map { |block| block[:block_type] }).to eq([0x09, 0x00, 0x0A, 0x01, 0x02, 0x02, 0x04, 0x05])
+    expect(blocks.map { |block| [block[:min_version], block[:current_version]] }).to eq(
+      [[1, 1], [1, 1], [0, 1], [1, 1], [1, 1], [1, 1], [1, 1], [2, 2]]
+    )
+    expect(blocks.all? { |block| block[:reserved] == 0 }).to be(true)
+  end
+
+  it "encodes point data with scene-space x coordinates and v2 point size" do
+    page = described_class.new
+    line = page.add_line
+    line.add_point(100, 200)
+    line.add_point(120, 220)
+
+    bytes = page.to_rm_bytes
+    line_block = parse_blocks(bytes).last
+    value_data = line_value_data(line_block)
+
+    _brush_tag, offset = parse_tag(value_data, 0)
+    offset += 4
+    _color_tag, offset = parse_tag(value_data, offset)
+    offset += 4
+    _thickness_tag, offset = parse_tag(value_data, offset)
+    offset += 8
+    _starting_length_tag, offset = parse_tag(value_data, offset)
+    offset += 4
+
+    points_data, offset = parse_subblock(value_data, offset, expected_index: 5)
+
+    expect(points_data.bytesize).to eq(28)
+
+    first_x, first_y = points_data.byteslice(0, 8).unpack("e2")
+    second_x, second_y = points_data.byteslice(14, 8).unpack("e2")
+    first_speed, first_width, first_direction, first_pressure = points_data.byteslice(8, 6).unpack("v v C C")
+
+    expect(first_x).to eq(-602.0)
+    expect(first_y).to eq(200.0)
+    expect(second_x).to eq(-582.0)
+    expect(second_y).to eq(220.0)
+    expect(first_speed).to eq(0)
+    expect(first_width).to eq(8)
+    expect(first_direction).to eq(0)
+    expect(first_pressure).to eq(255)
+
+    timestamp_tag, = parse_tag(value_data, offset)
+    expect(timestamp_tag).to eq(index: 6, type: 0x0F)
+  end
+
+  it "writes rgba tag 8 only for RGBA-colored lines" do
+    page = described_class.new
+
+    black_line = page.add_line
+    black_line.add_point(100, 200)
+
+    rgba_line = page.add_line
+    rgba_line.color = described_class::Colour::RGBA
+    rgba_line.rgba = 0xFF112233
+    rgba_line.add_point(140, 240)
+
+    blocks = parse_blocks(page.to_rm_bytes).select { |block| block[:block_type] == 0x05 }
+    black_value = line_value_data(blocks[0])
+    rgba_value = line_value_data(blocks[1])
+
+    expect(black_value).not_to include([8 << 4 | 0x04].pack("C"))
+    expect(rgba_value).to include([8 << 4 | 0x04].pack("C"))
+    expect(rgba_value).to include([0xFF112233].pack("V"))
+  end
+end
